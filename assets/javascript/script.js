6a7f3c53d97d83f7397a340637d7b1010973381c
Ren-Kun235 <changpobzeb651@gmail.com>
2023-03-28 20:03:36 -0500

    js starting layout

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,5 @@
-var button
+var startBtnEl = document.querySelector("#start");
+var highScrBtnEl = document.querySelector("#highScr")
+var timerEl = document.querySelector('#timer');
+var correctEl = document.querySelector('#correct');
+var incorrectEl = document.querySelector('#incorrect');
