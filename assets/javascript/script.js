21a2e571c99d68812fc5d5718e0b322d8472fe32
Ren-Kun235 <changpobzeb651@gmail.com>
2023-03-28 20:25:44 -0500

    configuring timer display and usage

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,22 @@
 var startBtnEl = document.querySelector("#start");
 var highScrBtnEl = document.querySelector("#highScr")
 var timerEl = document.querySelector('#timer');
-var correctEl = document.querySelector('#correct');
-var incorrectEl = document.querySelector('#incorrect');
+var corrEl = document.querySelector('#correct');
+var incorrEl = document.querySelector('#incorrect');
+
+var numBlanks = 0;
+var corrCounter = 0;
+var incorrCounter = 0;
+var timer = 90;
+var timerCount;
+
+function startTimer() {
+    timer = setInterval(function() {
+        timerEl. = timerCount;
+        if(corrCounter) {
+            timer++;
+        } else {
+            timer--;
+        }
+    }, 1000)
+}
