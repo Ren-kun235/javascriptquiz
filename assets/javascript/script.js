805e33643d3d15a38a02175487b9914a69bc3af9
Ren-Kun235 <changpobzeb651@gmail.com>
2023-04-02 19:40:12 -0500

    starter display/hide functions

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,19 +1,29 @@
 var startBtnEl = document.querySelector("#start");
-var highScrBtnEl = document.querySelector("#highScr")
+var startQuiz = document.querySelector("#")
+var highScrBtnEl = document.querySelector("#highScr");
+var highScrDisplay = document.querySelector("#highScores");
+var backToStart = document.querySelector("#backToStart");
+
 var timerEl = document.querySelector('#timer');
 var corrEl = document.querySelector('#correct');
 var incorrEl = document.querySelector('#incorrect');
 
-var numBlanks = 0;
 var corrCount = 0;
-var incorrCount = 0;
-var timer = 90;
-// var timerCount = math.floor((timeleft % (1000 * 60)) / 1000);
 
+highScrBtnEl.addEventListener('click', highScr)
 
+function highScr() {
+    highScrDisplay.classList.remove('hide')
+}
+
+backToStart.addEventListener('click', backStart);
+
+function backStart() {
+    highScrDisplay.classList.add('hide')
+}
 
 function startTimer() {
-    var timeLeft = 5;
+    var timeLeft = 90;
     timer = setInterval(function() {
 
         if(timeLeft > 1) {
