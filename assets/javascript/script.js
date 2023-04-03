4cb40ac2755e3514802edf454c5ba29bf0919a56
Ren-Kun235 <changpobzeb651@gmail.com>
2023-04-03 12:12:48 -0500

    configured button functions, timer positioning/display, HTML organization

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,26 +1,45 @@
 var startBtnEl = document.querySelector("#start");
-var startQuiz = document.querySelector("#")
+var startPage = document.querySelector("#startPage");
+var startQuiz = document.querySelector("#question1");
 var highScrBtnEl = document.querySelector("#highScr");
 var highScrDisplay = document.querySelector("#highScores");
 var backToStart = document.querySelector("#backToStart");
 
-var timerEl = document.querySelector('#timer');
-var corrEl = document.querySelector('#correct');
-var incorrEl = document.querySelector('#incorrect');
+var question1El = document.querySelector("#question1");
 
-var corrCount = 0;
+var timerEl = document.querySelector('#timer');
+// var corrEl = document.querySelector('#correct');
+// var incorrEl = document.querySelector('#incorrect');
+
+// var corrCount = 0;
+
+startBtnEl.addEventListener("click", question1);
+
+function question1() {
+    question1El.classList.remove("hide");
+    startPage.classList.add("hide");
+}
 
 highScrBtnEl.addEventListener('click', highScr)
 
 function highScr() {
-    highScrDisplay.classList.remove('hide')
+    highScrDisplay.classList.remove('hide');
+    startPage.classList.add("hide");
 }
 
 backToStart.addEventListener('click', backStart);
 
 function backStart() {
     highScrDisplay.classList.add('hide')
+    startPage.classList.remove("hide")
 }
+
+
+
+
+
+
+
 
 function startTimer() {
     var timeLeft = 90;
